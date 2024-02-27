f3d3489f6160c3f5f35d0712526a4b1679ae38cd
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-02-26 19:12:41 -0800

    remove unused interface impl's

--- src/main/java/ct/ContainerTweaksPlugin.java
+++ src/main/java/ct/ContainerTweaksPlugin.java
@@ -15,24 +15,4 @@ public class ContainerTweaksPlugin extends Plugin {
     public void onUnload() {
 
     }
-
-    @Override
-    public String getName() {
-        return "ContainerTweaks";
-    }
-
-    @Override
-    public String getVersion() {
-        return "1.3";
-    }
-
-    @Override
-    public String getDescription() {
-        return "Simple tweaks for moving items in containers";
-    }
-
-    @Override
-    public String[] getAuthors() {
-        return new String[]{"rfresh2"};
-    }
 }
